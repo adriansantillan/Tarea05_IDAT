d3acffda8ce0cb10c94a26fd3b98c3f16b24b629
FIRST_NAME LAST_NAME <adriansantillan2603@gmail.com>
2021-04-21 12:45:22 -0500

    se añadió el comboBox y función renderizar tabla

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
-let select = document.querySelector('#listadoCategoria');
+let seleccionCategoria = document.querySelector('#listadoCategoria');
+let nombreProducto = document.querySelector('#nombreProducto');
+let botonBuscar = document.querySelector('#botonBuscar');
 
-const obtenerAPI = () => {
+const llenarComboBox = () => {
+    
     fetch('https://disenoydesarrolloapi.azurewebsites.net/api/Producto/Categorias')
     .then(response => response.json())
-    .then(data => console.log(data.categorias[0]));
+    .then(data => data.categorias.forEach( (data) => {
+        seleccionCategoria.innerHTML += `<option> ${data} </option>`;
+    }));
+            
+   
 }    
-    
-const incluirCategorias = () =>{
-    let primeracategoria = obtenerAPI();    
-        select.innerHTML= '<option>' + primeracategoria +'</option>';
+   
+llenarComboBox();
+
+
+const buscarProducto = (event) => {
+    event.preventDefault ();
+    obtenerDatosAPI();
+    renderizarTabla();
 
 }
 
-select.addEventListener('click',incluirCategorias);
+const obtenerDatosAPI = () => {
+    const uri = 'https://disenoydesarrolloapi.azurewebsites.net/api/Producto';
+    let categoria = seleccionCategoria.value;
+    let producto = nombreProducto.value;
+    
+    let productoURI= uri + '?categoria=' + categoria + '&nombre=' + producto;
+    
+    fetch(productoURI)
+        .then (respuesta => respuesta.json())
+        .then (data =>
+                renderizarTabla(data)
+            );
+
+}
+
+const renderizarTabla = (data) => {
+    let contenidoTabla = document.querySelector('#contenidoTabla');
+
+    data.forEach (producto => {
+        contenidoTabla.innerHTML += '<tr><td>' + producto.codigo + '</td><td>' + producto.nombre + '</td><td>' + producto.categoria + '</td><td>' + producto.precio +'</td><td>' + producto.proveedor + '</td><td><button><img src="Iconos/modificar.png" width=20px height=20px></button></td><td><button><img src="Iconos/eliminar.jpg" width=20px height=20px></button></td></tr>'
+    });
+
+    
+
+}
+
+botonBuscar.addEventListener('click', buscarProducto);
 
